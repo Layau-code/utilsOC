52199e19101a8718b931522492706e3e03224fd3
赖尧 <yujitang_2006@qq.com>
2025-12-14 02:12:26 +0800

    v1-令牌桶-线程安全

--- TokenBucket/src/TokenBucketLimiter.java
+++ TokenBucket/src/TokenBucketLimiter.java
@@ -1,43 +1,63 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class TokenBucketLimiter implements RateLimiter{
 
     //桶的大小
-    private int capacity;
+    private final  int capacity;
     //每秒放入的令牌数
-    private int sRate;
+    private final int sRate;
 
     //当前令牌数量
-    private int curTokens;
+    private  AtomicInteger curTokens;
     //上次放入令牌的时间
-    private long lastTime;
+    private  AtomicLong lastTime;
 
     //构造函数
     public TokenBucketLimiter(int capacity, int sRate) {
+        if(capacity <= 0 || sRate <= 0){
+            throw new IllegalArgumentException("参数需要大于0");
+        }
         this.capacity = capacity;
         this.sRate = sRate;
-        this.curTokens = capacity;
-        this.lastTime = System.currentTimeMillis();
+        curTokens = new AtomicInteger(0);
+        lastTime = new AtomicLong(System.currentTimeMillis());
     }
 
     @Override
-    public boolean tryAcquire(int permits) {
+    public synchronized boolean  tryAcquire(int permits) {
         refill();
         //如果令牌数不足，则返回false
-        if (curTokens < permits) {
+        if(curTokens.get() < permits){
             return false;
         }
-        curTokens -= permits;
+        curTokens.addAndGet(-permits);
         return true;
     }
 
     @Override
-    public boolean tryAcquire() {
+    public   boolean tryAcquire() {
         return tryAcquire(1);
     }
-    public void refill(){
+    public synchronized void  refill(){
         long now = System.currentTimeMillis();
+        if(lastTime.get() > now){
+           return;
+        }
         //计算需要放入的令牌数
-        int needTokens = (int) ((now - lastTime)* sRate / 1000 );
-        curTokens = Math.min(curTokens + needTokens, capacity);
-        lastTime = now;
+        int addTokens = (int)((now - lastTime.get()) * sRate / 1000);
+        if(curTokens.get() + addTokens > capacity){
+            curTokens.set(capacity);
+        }else{
+            curTokens.addAndGet(addTokens);
+        }
+        lastTime.set(now);
+
+    }
+
+
+    public synchronized int getCurTokens(){
+        refill();
+        return curTokens.get();
     }
 }
